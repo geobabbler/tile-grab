c71dfdf492d7da885867cb06493d7f9d44381d74
Bill Dollins <bill@geomusings.com>
2017-05-08 09:19:15 -0400

    added shell scripts

--- scrape1.sh
+++ scrape1.sh
@@ -9,5 +9,3 @@ python tile_grab2.py -b "28.111;46.362;29.612;47.630" -z 12 -i false -d "moldova
 python tile_grab2.py -b "28.111;46.362;29.612;47.630" -z 13 -i false -d "moldova_satellite" -u "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BhdGlhbG5ldHdvcmtzIiwiYSI6ImNpcW83Mm1kYjAxZ3hmbm5ub2llYnNuMmkifQ.an57h9ykokxNlGArcWQztw" -f jpg
 
 python tile_grab2.py -b "28.111;46.362;29.612;47.630" -z 14 -i false -d "moldova_satellite" -u "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BhdGlhbG5ldHdvcmtzIiwiYSI6ImNpcW83Mm1kYjAxZ3hmbm5ub2llYnNuMmkifQ.an57h9ykokxNlGArcWQztw" -f jpg
-
-python tile_grab2.py -b "28.111;46.362;29.612;47.630" -z 15 -i false -d "moldova_satellite" -u "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BhdGlhbG5ldHdvcmtzIiwiYSI6ImNpcW83Mm1kYjAxZ3hmbm5ub2llYnNuMmkifQ.an57h9ykokxNlGArcWQztw" -f jpg
